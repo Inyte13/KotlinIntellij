91e225e2e2079dd331e818312cbbea908c0c24b2
Inyte13 <luisgardaz13@gmail.com>
2025-11-24 22:33:29 -0500

    arr

--- Teoria/src/Arr.kt
+++ Teoria/src/Arr.kt
@@ -1,6 +1,19 @@
 fun main() {
-  val nombres = arrayOf("Mafer", "Lucía", "Camila", "Paula", "Carlos")
-  for (i in nombres.indices) {
-    println("${i}: ${nombres[i]}")
-  }
+  // Arr estático
+  val numeros = arrayOf(0, 1, 2, 3, 4)
+
+  // println(numeros) no muestra el arr, muestra la referencia de memoria del arr
+  // println(numeros.contentToString())
+
+  // numeros[5], ArrayIndexOutOfBoundsException
+
+  // Reemplaza el value
+  numeros[2] = 7 // [0, 1, 7, 3, 4]
+
+  // numeros.lenght
+  numeros.size
+
+  // Recorriendo con forEach
+
+
 }
